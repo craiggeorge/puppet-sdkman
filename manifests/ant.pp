0c8a4445add1cbfe1306974cb5e00c3709a2de3a
Craig George <craig.george@orionhealth.com>
2016-07-16 21:02:00 +1200

    Add ability to fetch dependencies

--- manifests/ant.pp
+++ manifests/ant.pp
@@ -1,18 +1,30 @@
 define sdkman::ant(
   $version = $name,
-  $default = false
+  $default = false,
+  $dependencies,
 ) {
+
   require sdkman::install
 
   exec { "install-ant-$name":
     command => "bash --login -c 'sdk install ant ${version}'",
-    creates => "/Users/${::boxen_user}/.sdkman/candidates/ant/${version}"
+    creates => "/Users/${::boxen_user}/.sdkman/candidates/ant/${version}",
   }
 
-  if($default) {
+  if ($default) {
     exec { "set-ant-default":
       command => "bash --login -c 'sdk default ant ${version}'",
       require => Exec["install-ant-$name"],
     }
   }
+
+  if ($dependencies) {
+    exec { "fetch ant dependencies":
+      cwd     => "/Users/${boxen_user}/.sdkman/candidates/ant/${version}",
+      path    => ["/Users/${boxen_user}/.sdkman/candidates/ant/${version}/bin", "/usr/local/bin", "/usr/bin", "/bin", "/usr/sbin"],
+      command => "ant -f fetch.xml ${dependencies} -Ddest=system",
+      require => Exec["install-ant-$name"],
+    }
+  }
+
 }
